dc8aeba89ff6c76b7f7de702c1ede5ba9e100432
Thai-Son-TQ <thai-son.tran-quang@mmibordeaux.com>
2025-03-08 14:12:34 +0100

    feat: remet la mention d'horreur psychologique

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -11,7 +11,7 @@ const FeatureList = [
             Singularité
           </Heading>
           <p>
-            Singularité est un forum dystopique, fantastique et futuriste, dans un univers de notre conception à l’ambiance éclectique.
+            Singularité est un forum dystopique, fantastique et futuriste, dans un univers de notre conception à l’ambiance éclectique, centrée sur l’horreur psychologique.
             <br />
             Les grandes lignes de l’intrigue sont déjà tracées, mais malléables : le cadre est semi-évolutif.
             <br />
